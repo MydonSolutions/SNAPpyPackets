2a3e10dc2433a14e6dd53a2d1e8e9cfd42a29e9e
RocketRoss <code@radonn.co.za>
2020-11-08 05:08:58 +0000

    ^ send_udp: increment packet number.

--- send_udp.py
+++ send_udp.py
@@ -1,25 +1,32 @@
 import socket
 from SNAPPacket import SNAPPacket
+import time
 
-testPacket = SNAPPacket(
-    0,
-    True,
-    2,
-    2,
-    0,
-    3735928559,
-    [i % 16 for i in range(16*2*2)]
+channels = 256
+
+packet1 = SNAPPacket(
+    0,          #fwVersion
+    True,       #packetType is voltage
+    channels,   #channels
+    0,          #channelNum
+    0,          #fEngineId
+    0,          #sampleNumber        
+    [int(i/16) % 16 for i in range(16*2*2*channels)]
 )
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 4015
-MESSAGE = testPacket.packet()
 
 print("UDP target IP: %s" % UDP_IP)
 print("UDP target port: %s" % UDP_PORT)
 # print("message: %s" % MESSAGE)
-testPacket.print()
+packet1.print()
 
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+packet1.print()
+while True:
+    sock.sendto(packet1.packet(), (UDP_IP, UDP_PORT))
+    packet1.update(packetNumber=packet1.packetNumber+16)
+    time.sleep(0.0001)
